8465d0dfc309b2f9a18980cb51635d781d481f67
hainam1411 <hainam141199@gmail.com>
2024-06-19 13:45:56 +0700

    index

---
+++ src/main/java/org/example/customermanagementjpa/service/IGenerateService.java
@@ -0,0 +1,14 @@
+package org.example.customermanagementjpa.service;
+
+import org.example.customermanagementjpa.model.Customer;
+import java.util.List;
+
+public interface IGenerateService<T> {
+    List<T> findAll();
+
+    void save(T t);
+
+    T findById(Long id);
+
+    void remove(Long id);
+}
